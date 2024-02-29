51cd441bbc2f55216b84be6354028848022d0008
klu-2200090053 <2200090053@kluniversity.in>
2024-02-29 14:44:05 +0530

    Pushing two java files into Repo

---
+++ Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+	public static void main(String[] args){
+		System.out.println("Welcome User!");
+		System.out.println("Lab In Sem Exam - CDD");
+	}
+}
+
---
+++ Basic2.java
@@ -0,0 +1,11 @@
+public class Basic2{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 20;
+		int res = a + b;
+		System.out.println("Addition = ");
+		System.out.println("A = " + a + " and B = " + b);
+		System.out.println("Total : " + res);
+	}
+}
+
